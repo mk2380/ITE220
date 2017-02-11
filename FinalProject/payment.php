e6e64c3287e5c5afbecba10625fd5d44066dfab2
Michael Kwok <weizheng.guo@students.stamford.edu>
2017-02-11 16:02:59 +0700

    Final Project

---
+++ FinalProject/payment.php
@@ -0,0 +1,31 @@
+<?php include("header.php") ?>
+<!-- Type your code here -->
+<br />
+
+	<div class="container">
+		<div class="row">
+		
+			<div class="col-md-12 text-center">
+			<h1>How to pay?</h1>
+			<p>You can transfer the money to these following account,</p>
+			<br />
+    			<a class=""><img src="https://www.mx7.com/i/cfe/9PDkMS.jpg" height="300" weight="100">
+    			</a>
+    		</div>
+    </div>
+    <br />
+<div class="row">
+		
+			<div class="col-md-12 text-center">
+			<p>or you can pay by Paypal through our system by using</p>
+			<br />
+    			<a class=""><img src="http://logodatabases.com/wp-content/uploads/2012/03/paypal-logo-wallpaper.jpg" height="100" width="200">
+    			</a>
+    		</div>
+
+    	</div>
+    </div>
+    <br />
+
+
+<?php include("footer.php") ?>
---
+++ FinalProject/product.js
@@ -0,0 +1,16 @@
+var game = {
+	products : ["TOM CLANCYS GHOST RECON WILDLANDS", "Sniper Elite4, Resident Evil7", "Nioh, Naruto4 Road to Boruto", "Dead Rising4", "Dying Light", "Fifa17", "For Honor", "Gears Of Wars4", "Hitman", "Mafia III"],
+	prices: [1790, 1890, 1890, 1990, 1690, 1590, 1290, 1790, 2190, 1890, 1490],
+	
+	displayProduct: function(){
+		var productsText = " "; 
+		var productsElement = document.getElementById("product_form");
+		for(var i=0; i< products.length; i++){
+			productsText += "<form action='https://www.paypal.com/cgi-bin/webscr' method='post'><div class='card' style='width: 20rem'><img class='card-img-top' src='img/" + (i+1) + ".jpg' alt='Card image cap'><div class='card-block'><h4 class='card-title'>" + this.products[i] + "</h4><p class='card-text'>Price: " + this.prices[i] + "Baths</p><input type='hidden' name='cmd' value='_cart' /><input type='hidden' name='add' value='1' /><input type='hidden' name='business' value='payment@nextlevel.com' /><input type='hidden' name='item_name' value='" + this.products[i] + "' /><input type='hidden' name='quantity' value='1' /><input type='hidden' name='amount' value='" + this.prices[i] +"' /><input type='hidden' name='currency_code' value='THB' /><input type='hidden' name='no_note' value='1'><input type='hidden' name='shipping' value='50'><input type='submit' name='submit' value='Add to cart' class='btn btn-primary' /></div></div></form>"
+		};
+		
+		productsElement.innerHTML = productsText;
+
+	}
+
+game.displayProduct();
